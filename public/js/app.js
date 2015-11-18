21ec56b12651819a007c417cfa90f5b34ca26d1b
Bookis Smuin <bookis.smuin@gmail.com>
2015-11-18 15:42:14 -0800

    Delete unused files and style mobile

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
 $(function() {
-  
+  $("[data-scroll-to]").click(function() {
+    $("html, body").animate({
+      scrollTop: $($(this).data("scroll-to")).offset().top
+    }, 300)
+    return false;
+  });
 });
