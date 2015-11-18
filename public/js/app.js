620aafd0c866ceddaadb3224e4fbee9db561f970
Bookis Smuin <bookis.smuin@gmail.com>
2015-11-18 15:53:59 -0800

    Make animated underline added via js to sync

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
 $(function() {
+
+  $(".about-wrapper .columns").addClass("animated-underline");
+
   $("[data-scroll-to]").click(function() {
     $("html, body").animate({
       scrollTop: $($(this).data("scroll-to")).offset().top
